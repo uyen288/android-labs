c3de161a06a3c20344157bff7200428734e1ad86
wildfire889 <hohuonguyen0609@gmail.com>
2026-02-06 21:50:35 +0700

    Last Changes on LifecycleDemo

--- LifecycleDemo/app/src/main/java/com/example/lifecycledemo/SubActivity.java
+++ LifecycleDemo/app/src/main/java/com/example/lifecycledemo/SubActivity.java
@@ -1,6 +1,16 @@
 package com.example.lifecycledemo;
 
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SubActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sub);
+    }
 }
