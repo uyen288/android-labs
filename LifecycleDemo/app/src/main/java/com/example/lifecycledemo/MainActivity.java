6ca7c8391b65636ab3bc8ca8619bb75feb290399
wildfire889 <hohuonguyen0609@gmail.com>
2026-02-06 21:39:42 +0700

    Last Changes on LifecycleDemo

--- LifecycleDemo/app/src/main/java/com/example/lifecycledemo/MainActivity.java
+++ LifecycleDemo/app/src/main/java/com/example/lifecycledemo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lifecycledemo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,69 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnExplicit;
+    Button btnImplicit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Log.d("Lifecycle", "Hàm onCreate được gọi");
+
+        event();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Lifecycle", "Hàm onStart được gọi");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Lifecycle", "Hàm onResume được gọi");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Lifecycle", "Hàm onPause được gọi");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Lifecycle", "Hàm onStop được gọi");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Lifecycle", "Hàm onDestroy được gọi");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Lifecycle", "Hàm onRestart được gọi");
+    }
+
+    private void event() {
+        btnExplicit = findViewById(R.id.btn_explicit);
+        btnImplicit = findViewById(R.id.btn_implicit);
+
+        btnExplicit.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SubActivity.class);
+            startActivity(intent);
+        });
+
+        btnImplicit.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://google.com"));
+            startActivity(intent);
         });
     }
+
 }
---
+++ LifecycleDemo/app/src/main/java/com/example/lifecycledemo/SubActivity.java
@@ -0,0 +1,6 @@
+package com.example.lifecycledemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+}
