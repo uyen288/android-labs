2f7e8e1612fe47a6fd4f245b12cb7e89572cec40
wildfire889 <hohuonguyen0609@gmail.com>
2026-03-12 22:32:28 +0700

    Upd on Lab 8

---
+++ Lab8_TabLayout/app/src/main/java/com/example/lab8_tablayout/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lab8_tablayout;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0: tab.setText("Home"); break;
+                case 1: tab.setText("Favourite"); break;
+                case 2: tab.setText("Setting"); break;
+            }
+        }).attach();
+    }
+}
